def405093ed7642757d22056ff202ff13cf954d3
GuuilhermeR <guilherme.rudiger@hotmail.com>
2024-03-24 20:08:15 -0300

    C#: Aplicando a Orientação a Objetos: 01.Classe e Método em C#

--- ScreenSound/Musica.cs
+++ ScreenSound/Musica.cs
@@ -1,7 +1,23 @@
 ﻿class Musica
 {
-    string nome;
-    string artista;
-    int duracao;
-    bool disponivel;
+    public string nome;
+    public string artista;
+    public int duracao;
+    public bool disponivel;
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponivel)
+        {
+            Console.WriteLine("Disponível no plano.");
+        }
+        else
+        {
+            Console.WriteLine("Adquira o plano Plus+.");
+        }
+    }
+
 }
