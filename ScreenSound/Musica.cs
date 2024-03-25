62c74aa331500fe88e48aca5dca01c3d17b6a454
GuuilhermeR <guilherme.rudiger@hotmail.com>
2024-03-25 20:22:17 -0300

    C#: aplicando a Orientação a Objetos - 05. Atributos e Propriedades e 06.Alterando o GET com lambda.

--- ScreenSound/Musica.cs
+++ ScreenSound/Musica.cs
@@ -1,16 +1,18 @@
 ﻿class Musica
 {
-    public string nome;
-    public string artista;
-    public int duracao;
+    public string Nome { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
     public bool Disponivel { get; set; }
+    public string DescricaoResumida => $"A música {Nome} pertence à banda {Artista}"; //propriedade somente leitura pode ser definida como aron fuction (lambda)
 
 
     public void ExibirFichaTecnica()
     {
-        Console.WriteLine($"Nome: {nome}");
-        Console.WriteLine($"Artista: {artista}");
-        Console.WriteLine($"Duração: {duracao}");
+        Console.WriteLine(DescricaoResumida);
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duração: {Duracao}");
         if (Disponivel)
         {
             Console.WriteLine("Disponível no plano.");
